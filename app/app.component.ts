a2e702eabe99ce43ccafa0d213d941b3bfcd279d
John Wu <me@johnwu.cc>
2017-06-13 12:14:59 +0800

    add sample

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,21 @@ import { Component, VERSION } from "@angular/core";
 
 @Component({
     selector: "my-app",
-    templateUrl: "/app/app.component.html"
+    templateUrl: "/app/app.component.html",
+    styleUrls: ["app/app.component.css"]
 })
 export class AppComponent {
     name = "Angular";
     version = VERSION.full;
+    counter: number = 0;
+
+    constructor() {
+        setInterval(() => {
+            this.counter++;
+        }, 1000);
+    }
+
+    onClickButton(): void {
+        alert("Hello " + this.name);
+    }
 }
